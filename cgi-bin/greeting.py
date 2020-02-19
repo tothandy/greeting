572b571d1f603c7bf439b7a23e6c1a8a759ab009
Andy Toth <ondrej.toth@code.berlin>
2020-02-19 15:39:25 +0100

    greeting

---
+++ cgi-bin/greeting.py
@@ -0,0 +1,21 @@
+print("""
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+<p>
+    <form action="" method="get" class="form-example">
+      <div class="form-example">
+      <label for="name">Enter your name: </label>
+      <input type="text" name="name" id="name" required>
+      </div>
+    </form>
+</p>
+</body>
+
+</html>
+""")
